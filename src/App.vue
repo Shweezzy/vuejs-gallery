b95bba5e44156fc755f26114a69494450371bbdd
Shweezzy <56113725+Shweezzy@users.noreply.github.com>
2020-08-10 16:11:53 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+#googlePhoto {
+  max-width: 230px;
+  width: 100%;
+}
+#switchSpan {
+  color: #007bff;
+  vertical-align: middle;
+}
+input[type="file"] {
+  display: none;
+}
+#addBtn {
+  border: none;
+}
+#switch {
+  float: inherit;
+  width: 90px;
+}
+.row {
+  margin: 0;
+}
+.col-sm {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.col-sm-6 {
+  text-align: center;
+}
+.btn-secondary {
+  opacity: 0.6;
+}
+.fileupload {
+  padding: 0.4em 1em 0.4em 1em;
+  border-radius: 0.3em;
+  margin: 0;
+  color: #007bff;
+}
+#cardText {
+  text-align: center;
+  opacity: 0.6;
+  padding-bottom: 5%;
+}
+.fileupload:hover {
+  cursor: pointer;
+  color: white;
+  background-color: #007bff;
+}
+.custom-file-upload {
+  display: inline-block;
+  padding: 6px 12px;
+  cursor: pointer;
+}
+.form-inline {
+  width: 100%;
+}
+#__BVID__5 {
+  width: 100%;
+}
+#file-selector__BV_file_outer_ {
+  width: 90%;
+}
+#carousel-1 {
+  width: 600px;
+}
+#carousel-image {
+  height: 500px;
+}
+#carousel {
+  display: flex;
+  justify-content: center;
+}
+.card-body {
+  padding: 0;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,270 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="row align-items-center">
+      <div class="col-sm">
+        <img
+          id="googlePhoto"
+          src="@/images/logo.png"
+          class="d-inline-block align-top"
+          alt="google Logo"
+        />
+      </div>
+      <div class="col-sm-6">
+        <span id="switchSpan">Switch to</span>
+        <br />
+        <b-button @click="switchList" id="switch" variant="outline-primary"
+          >List</b-button
+        >
+        <b-button @click="switchCarousel" id="switch" variant="outline-primary"
+          >Carousel</b-button
+        >
+      </div>
+      <div class="col-sm">
+        <label class="fileupload">
+          <input id="file-selector" ref="file" type="file" multiple /><b-icon
+            icon="camera"
+          ></b-icon>
+          Choose
+        </label>
+      </div>
+      <div class="col-sm">
+        <b-button
+          id="addBtn"
+          @click="handleFileUpload"
+          variant="outline-primary"
+          ><b-icon-arrow-up></b-icon-arrow-up> Send</b-button
+        >
+      </div>
+    </div>
+    <hr />
+    <div class="row d-flex flex-wrap" v-if="show">
+      <div
+        class="w-25 p-3"
+        v-for="(url, index) in photosList"
+        :key="index"
+        deck
+      >
+        <b-card
+          id="listPhoto"
+          :img-src="
+            `https://photo-album-hoza.s3.ap-northeast-2.amazonaws.com/` + url
+          "
+          img-alt="Image"
+          img-top
+        >
+          <b-card-text id="cardText">
+            {{ url }}
+            <br />
+            <form
+              :action="
+                `https://photo-album-hoza.s3.ap-northeast-2.amazonaws.com/` +
+                  url
+              "
+              target="_blank"
+            >
+              <button class="btn btn-secondary">
+                <b-icon icon="cloud-download" aria-hidden="true"></b-icon>
+              </button>
+              <b-button class="btn" @click="deletePhoto(url)"
+                ><b-icon icon="trash-fill" aria-hidden="true"></b-icon
+              ></b-button>
+            </form>
+          </b-card-text>
+        </b-card>
+      </div>
+    </div>
+    <div v-else id="carousel">
+      <b-carousel
+        id="carousel-1"
+        v-model="slide"
+        :interval="4000"
+        controls
+        indicators
+        background="#ababab"
+        img-width="500"
+        img-height="480"
+        style="text-shadow: 1px 1px 2px #333;"
+        @sliding-start="onSlideStart"
+        @sliding-end="onSlideEnd"
+      >
+        <b-carousel-slide v-for="(url, index) in photosList" :key="index">
+          <template v-slot:img>
+            <img
+              id="carousel-image"
+              class="d-block img-fluid w-100"
+              :src="
+                `https://photo-album-hoza.s3.ap-northeast-2.amazonaws.com/` +
+                  url
+              "
+              alt="image slot"
+            />
+          </template>
+          <form
+            :action="
+              `https://photo-album-hoza.s3.ap-northeast-2.amazonaws.com/` + url
+            "
+            target="_blank"
+          >
+            <button class="btn btn-secondary">
+              <b-icon icon="cloud-download" aria-hidden="true"></b-icon>
+            </button>
+            <b-button @click="deletePhoto(url)"
+              ><b-icon icon="trash-fill" aria-hidden="true"></b-icon
+            ></b-button>
+          </form>
+        </b-carousel-slide>
+      </b-carousel>
+    </div>
   </div>
 </template>
 
 <script>
+import AWS from "aws-sdk";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      file: null,
+      albumBucketName: "photo-album-hoza",
+      bucketRegion: "ap-northeast-2",
+      IdentityPoolId: "ap-northeast-2:c9123c64-387a-48b1-bf8f-78eccd3fd928",
+      photosList: [],
+      slide: 0,
+      sliding: null,
+      show: true,
+    };
+  },
+  created() {
+    this.getFiles();
+  },
+  methods: {
+    switchCarousel() {
+      return (this.show = false);
+    },
+    switchList() {
+      return (this.show = true);
+    },
+    downloadPhoto(name) {
+      return `https://photo-album-hoza.s3.ap-northeast-2.amazonaws.com/${name}`;
+    },
+    onSlideStart() {
+      this.sliding = true;
+    },
+    onSlideEnd() {
+      this.sliding = false;
+    },
+    handleFileUpload() {
+      for (let i = 0; i < this.$refs.file.files.length; i++) {
+        this.file = this.$refs.file.files[i];
+        this.uploadPhoto();
+      }
+    },
+    uploadPhoto() {
+      AWS.config.update({
+        region: this.bucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: this.IdentityPoolId,
+        }),
+      });
+
+      var s3 = new AWS.S3({
+        apiVersion: "2006-03-01",
+        params: {
+          Bucket: this.albumBucketName,
+        },
+      });
+
+      let photoKey = this.file.name;
+
+      s3.upload(
+        {
+          Key: photoKey,
+          Body: this.file,
+          ACL: "public-read",
+        },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+            return alert(
+              "There was an error uploading your photo: ",
+              err.message
+            );
+          }
+          this.getFiles();
+          console.log(data);
+        }
+      );
+    },
+    getFiles() {
+      AWS.config.update({
+        region: this.bucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: this.IdentityPoolId,
+        }),
+      });
+
+      var s3 = new AWS.S3({
+        apiVersion: "2006-03-01",
+        params: {
+          Bucket: this.albumBucketName,
+        },
+      });
+
+      s3.listObjects(
+        {
+          Delimiter: "/",
+        },
+        (err, data) => {
+          if (err) {
+            return alert(
+              "There was an error listing your albums: " + err.message
+            );
+          } else {
+            this.photosList.length = 0;
+            Object.values(data.Contents).forEach((e) => {
+              this.photosList.push(e.Key);
+            });
+            console.log(data.Contents);
+          }
+        }
+      );
+    },
+    deletePhoto(key) {
+      AWS.config.update({
+        region: this.bucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: this.IdentityPoolId,
+        }),
+      });
+
+      var s3 = new AWS.S3({
+        apiVersion: "2006-03-01",
+        params: {
+          Bucket: this.albumBucketName,
+        },
+      });
+
+      s3.deleteObject(
+        {
+          Key: key,
+        },
+        (err, data) => {
+          if (err) {
+            return alert(
+              "There was an error deleting your photo: ",
+              err.message
+            );
+          }
+          alert("Successfully deleted photo.");
+          this.getFiles();
+          console.log(data);
+        }
+      );
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style></style>
